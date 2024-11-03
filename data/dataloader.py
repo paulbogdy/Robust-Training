a860388b7b255a90960a4f9ebf811e741d1f5631
Paul <jurcutpaul492001@gmail.com>
2024-11-03 14:52:30 +0100

    Fixed dataset loading bug

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,12 +1,9 @@
-from datasets import load_dataset
+import datasets
 
 def load_dataset(dataset_name: str):
     if dataset_name == 'sst':
-        dataset = load_dataset('glue', 'sst2')
+        dataset = datasets.load_dataset('glue', 'sst2')
         dataset = dataset.map(lambda x: {'sentence': x['sentence'], 'label': x['label']})
-    elif dataset_name == 'imdb':
-        dataset = load_dataset('imdb')
-        dataset = dataset.map(lambda x: {'sentence': x['text'], 'label': x['label']})
     else:
         raise ValueError(f"Dataset {dataset_name} not supported")
     
