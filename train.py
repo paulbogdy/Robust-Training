2e58b56aac3702af65bc236e41b55a41f2c7525c
Paul <jurcutpaul492001@gmail.com>
2024-11-17 22:48:45 +0100

    Added slightly better alligned contrastive loss

--- train.py
+++ train.py
@@ -56,6 +56,8 @@ def main(args):
         trainer = ContrastiveV4Trainer(model_wrapper, device, args)
     elif args.training_method == 'contrastive_v5':
         trainer = ContrastiveV5Trainer(model_wrapper, get_alphabet(args.dataset_name), device, args)
+    elif args.training_method == 'contrastive_v6':
+        trainer = ContrastiveV6Trainer(model_wrapper, get_alphabet(args.dataset_name), device, args)
     elif args.training_method == 'rand_mask':
         trainer = RandMaskTrainer(model_wrapper, device, args)
     elif args.training_method == 'base':
@@ -89,7 +91,7 @@ if __name__ == "__main__":
     parser.add_argument(
         '--training_method', 
         type=str, 
-        choices=['adv_emb', 'rand_char', 'rand_char_v2', 'rand_char_v3', 'rand_char_v4', 'base', 'contrastive', 'contrastive_v2', 'contrastive_v3', 'contrastive_v4', 'contrastive_v5', 'rand_mask'], 
+        choices=['adv_emb', 'rand_char', 'rand_char_v2', 'rand_char_v3', 'rand_char_v4', 'base', 'contrastive', 'contrastive_v2', 'contrastive_v3', 'contrastive_v4', 'contrastive_v5', 'contrastive_v6', 'rand_mask'], 
         required=True,
         help='Training method to use.')
     parser.add_argument(
@@ -130,6 +132,8 @@ if __name__ == "__main__":
         parser = ContrastiveV4Trainer.add_args(parser)
     elif args.training_method == 'contrastive_v5':
         parser = ContrastiveV5Trainer.add_args(parser)
+    elif args.training_method == 'contrastive_v6':
+        parser = ContrastiveV6Trainer.add_args(parser)
     elif args.training_method == 'rand_mask':
         parser = RandMaskTrainer.add_args(parser)
 
--- trainers/__init__.py
+++ trainers/__init__.py
@@ -9,4 +9,5 @@ from trainers.contrastive_v2 import ContrastiveV2Trainer
 from trainers.contrastive_v3 import ContrastiveV3Trainer
 from trainers.contrastive_v4 import ContrastiveV4Trainer
 from trainers.contrastive_v5 import ContrastiveV5Trainer
+from trainers.contrastive_v6 import ContrastiveV6Trainer
 from trainers.rand_mask import RandMaskTrainer
